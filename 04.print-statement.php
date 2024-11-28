45b5fcda732e9867008bc3acf73bdcf0f17966c8
Carlos Vesta <vesta.carlos@gmail.com>
2024-11-28 21:41:26 +0200

    04.print-statement

--- 04.print-statement.php
+++ 04.print-statement.php
@@ -3,16 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>The Echo Statement</title>
+    <title>The Print Statement</title>
 </head>
 <body>
-    <?php
-        echo "The Echo Statement.<br>";
+    <?php 
 
-        $nome = "Carlos";
+        print "Hello " . "World!";
 
-        echo "Olá {$nome}";
     ?>
-    
 </body>
 </html>
