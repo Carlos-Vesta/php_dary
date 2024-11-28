a518e9a4ff62540567b65a93702062035d4da6ba
Carlos Vesta <vesta.carlos@gmail.com>
2024-11-28 21:48:30 +0200

    05.comments

---
+++ 05.comments.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comments in PHP</title>
+</head>
+<body>
+    <?php
+        // Comentário em uma linha
+
+        # Comentário em uma linha
+
+        /*
+            Comentário em
+            multiplas 
+            linhas
+        */
+
+        $idade = 35;
+        $nome = "Carlos Vesta";
+
+        echo "Olá {$nome}, você tem {$idade} anos de idade.";
+
+    ?>
+    
+</body>
+</html>
