7d09d867228a4a39aa3e02be2bc90a25085d668d
Carlos Vesta <vesta.carlos@gmail.com>
2024-11-28 21:33:26 +0200

    03.echo-statement

---
+++ 03.echo-statement.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>The Echo Statement</title>
+</head>
+<body>
+    <?php
+        echo "The Echo Statement.<br>"."<br>";
+
+        $nome = "Carlos";
+
+        echo "Olá {$nome}";
+    ?>
+    
+</body>
+</html>
