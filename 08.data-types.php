86215c038d0b90c953a2acc58c2a0abf7e942604
Carlos Vesta <vesta.carlos@gmail.com>
2024-12-06 22:22:06 +0200

    08.data-types

---
+++ 08.data-types.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Types</title>
+</head>
+<body>
+    <h1>Data Types</h1>
+    <?php
+    /**
+     * INTEGER = Numeric values;
+     * FLOATING POINT OR FLOAT = Decimal numbers;
+     * STRING = Text;
+     * BOOLEANS = True / False;
+     * NULL = Null;
+     * 
+     * 
+     * ARRAY;
+     * OBJECT
+     * RESOURCE
+     */
+
+     $age = 36; // integer
+     echo "Eu tenho {$age} anos de idade. <br>";
+
+     $price = 22.5; // float
+     echo "O preço é {$price}. <br>";
+       
+     $name = "Carlos Vesta"; // string
+     echo "O meu nome é {$name}. <br>";
+
+     $is_allowed = true; // boolean
+     echo "$is_allowed";
+
+    $gender; // Null
+
+    ?>
+    
+</body>
+</html>
